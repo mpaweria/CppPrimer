01ef0c3ccebf6e532eb01614988c145f5a5b925e
mpaweria <ms.paweria@gmail.com>
2021-05-19 22:42:37 +0530

    Solutions till 3.2

--- Chapter3.cpp
+++ Chapter3.cpp
@@ -20,46 +20,74 @@ Exercise 3.1: Rewrite the exercises from § 1.4.1 (p. 13) and § 2.6.2 (p. 76) w
 // }
 
 //  2.6.2
-#include<iostream>
-#include<string>
-using namespace std;
+// #include<iostream>
+// #include<string>
+// using namespace std;
 
-struct Sales_data
-{
-    string isbn;
-    unsigned unitSold=0;
-    double price=0.0;
-    double revenue=0.0; 
-};
-int main()
-{
-    int count=1;
-    double totalRevenue;
-    Sales_data item1, item2;
-    cout << "Enter the details of the books:"<<endl;
-    cin >> item1.isbn >> item1.unitSold >> item1.price;
-    cin >> item2.isbn >> item2.unitSold >> item2.price;
-    item1.revenue = item1.unitSold * item1.price;
-    item2.revenue = item2.unitSold * item2.price;
-    if(item1.isbn == item2.isbn)
-    {
-        totalRevenue = item1.revenue + item2.revenue;
-        count = item1.unitSold + item2.unitSold;
-        cout << item1.isbn << " " << count << " " << totalRevenue << endl;
+// struct Sales_data
+// {
+//     string isbn;
+//     unsigned unitSold=0;
+//     double price=0.0;
+//     double revenue=0.0; 
+// };
+// int main()
+// {
+//     int count=1;
+//     double totalRevenue;
+//     Sales_data item1, item2;
+//     cout << "Enter the details of the books:"<<endl;
+//     cin >> item1.isbn >> item1.unitSold >> item1.price;
+//     cin >> item2.isbn >> item2.unitSold >> item2.price;
+//     item1.revenue = item1.unitSold * item1.price;
+//     item2.revenue = item2.unitSold * item2.price;
+//     if(item1.isbn == item2.isbn)
+//     {
+//         totalRevenue = item1.revenue + item2.revenue;
+//         count = item1.unitSold + item2.unitSold;
+//         cout << item1.isbn << " " << count << " " << totalRevenue << endl;
 
-        if(count!=0)
-        {
-            cout << "Average price of the book " << totalRevenue/count;
-        }
-        else 
-        {
-            cout << "No Sales.";
-        }
-        return 0;
-    }
-    else 
-    {
-        cerr << "Data must refer to same ISBN. ";
-        return -1;
-    }
-}
+//         if(count!=0)
+//         {
+//             cout << "Average price of the book " << totalRevenue/count;
+//         }
+//         else 
+//         {
+//             cout << "No Sales.";
+//         }
+//         return 0;
+//     }
+//     else 
+//     {
+//         cerr << "Data must refer to same ISBN. ";
+//         return -1;
+//     }
+// }
+
+
+/*-------------------------------------------------3.1---------------------------------------
+
+Exercise 3.2: Write a program to read the standard input a line at a time. Modify your program to read a word at a time.*/
+
+//  One line at a time
+// #include<iostream>
+// #include<string>
+// using namespace std;
+
+// int main()
+// {
+//     for(string str; getline(cin, str);){
+//         cout << str << endl;
+//     }
+// }
+
+//  One word at a time
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//     for(string str; cin >> str;){
+//         cout << str << endl;
+//     }
+// }
