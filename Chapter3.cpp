c12dd62b192d2a80ad9cb520fdf179d41398b932
mpaweria <ms.paweria@gmail.com>
2021-05-20 23:37:12 +0530

    Solutions till 3.5

--- Chapter3.cpp
+++ Chapter3.cpp
@@ -91,3 +91,104 @@ Exercise 3.2: Write a program to read the standard input a line at a time. Modif
 //         cout << str << endl;
 //     }
 // }
+
+
+/*  Exercise 3.3: Explain how whitespace characters are handled in the string input operator and in the getline function.  */
+
+/*  String input operator considers a space as a terminating character, which means that it can only display a single 
+    word (even if you type many words).
+
+    The getline function takes an input stream and a string. This function reads the given stream up to and including 
+    the first newline and stores what it read—not including the newline—in its string argument. After getline sees a 
+    newline, even if it is the first character in the input, it stops reading and returns. If the first character in 
+    the input is a newline, then the resulting string is the empty string.   */
+
+
+/*  Exercise 3.4: Write a program to read two strings and report whether the strings are equal. If not, report which of 
+    the two is larger. Now, change the program to report whether the strings have the same length, and if not, 
+    report which is longer.  */
+
+// #include<iostream>
+// #include<string>
+// using namespace std;
+
+// int main()
+// {
+//     string s1, s2;
+//     cout << "Enter string 1: ";
+//     getline(cin, s1);
+//     cout << "Enter string 2: ";
+//     getline(cin, s2);
+
+//     // Check larger string
+//     if(s1==s2)
+//     {
+//         cout << "Strings " << s1 << " and " << s2 << " are equal." << endl;
+//     }
+//     else if(s1>s2)
+//     {
+//         cout << "String " << s1 << " is larger than " << s2 << endl;
+//     }
+//     else 
+//     {
+//         cout << "String " << s2 << " is larger than " << s1 << endl;
+//     }
+
+//     // Check larger string
+//     if(s1.size()==s2.size())
+//     {
+//         cout << "Strings " << s1 << " and " << s2 << " are equal." << endl;
+//     }
+//     else if(s1.size()>s2.size())
+//     {
+//         cout << "String " << s1 << " is longer than " << s2 << endl;
+//     }
+//     else 
+//     {
+//         cout << "String " << s2 << " is longer than " << s1 << endl;
+//     }
+//     return 0;
+// }
+
+
+/*  Exercise 3.5: Write a program to read strings from the standard input, concatenating what is read into one large string.
+    Print the concatenated string. Next, change the program to separate adjacent input strings by a space.  */
+
+// #include<iostream>
+// #include<string>
+// using namespace std;
+
+// int main()
+// {
+//     string str, s;
+//     while(cin>>s)
+//     {
+//         str+=s;
+//     }
+//     cout << "The concatenated string is " << str << endl;
+//     return 0;
+// }
+
+
+// #include<iostream>
+// #include<string>
+// using namespace std;
+
+// int main()
+// {
+//     string str, s;
+//     while(cin >> s)
+//     {
+//         if(str.empty())
+//         {
+//             str+=s;
+//         }
+//         else
+//         {
+//             str += " " + s;
+//         }
+//     }
+//     cout << "The concatenated string is " << str << endl;
+
+//     return 0;
+// }
