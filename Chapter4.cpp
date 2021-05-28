f6c40ffc064a7a5a1f638609a231a4681a85e02a
mpaweria <ms.paweria@gmail.com>
2021-05-28 17:58:29 +0530

    Solutions till 4.12

--- Chapter4.cpp
+++ Chapter4.cpp
@@ -54,3 +54,40 @@ expression (without parentheses) and printing its result.
     short svalue = 32767; ++svalue; // -32768
     unsigned uivalue = 0; --uivalue;  // 4294967295
     unsigned short usvalue = 65535; ++usvalue;  // 0    */
+
+
+/*----------------------------------------------------4.3-------------------------------------------------------------
+
+Exercise 4.8: Explain when operands are evaluated in the logical AND, logical OR, and equality operators.   */
+/*  The logical AND and OR operators always evaluate their left operand before the right. Moreover, the right 
+    operand is evaluated if and only if the left operand does not determine the result. This strategy is known 
+    as short-circuit evaluation.
+    logical AND : the second operand is evaluated if and only if the left side is true.
+    logical OR : the second operand is evaluated if and only if the left side is false
+    equality operators == : undefined.      */
+
+
+/*  Exercise 4.9: Explain the behavior of the condition in the following if:
+    const char *cp = "Hello World";
+    if (cp && *cp)      */
+
+/*  cp is a pointer to const char and is not a nullptr, i.e true.
+    *cp is a const char'H', it is a nonzero value, i.e true.
+    true && true -> true.     */ 
+
+
+/*  Exercise 4.10: Write the condition for a while loop that would read ints from the standard input and stop
+    when the value read is equal to 42.    */
+
+/*  int n;
+    while(cin>n && n!=42)   */
+
+
+/*  Exercise 4.11: Write an expression that tests four values, a, b, c, and d, and ensures that a is greater than b, 
+    which is greater than c, which is greater than d.   */    
+//  a>b && b>c && c>d
+
+
+//  Exercise 4.12: Assuming i, j, and k are all ints, explain what i != j < k means.
+    
+//  i != j < k means i!=(j<k) --> i != 0 or 1.
