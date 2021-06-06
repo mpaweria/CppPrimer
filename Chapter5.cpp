d2121b1ec6c929bce88d0fb79977783bec454658
mpaweria <ms.paweria@gmail.com>
2021-06-06 23:59:05 +0530

    Solutions till 5.13

--- Chapter5.cpp
+++ Chapter5.cpp
@@ -352,3 +352,109 @@ Exercise 5.9: Write a program using a series of if statements to count the numbe
 // 	return 0;
 // }
 
+
+
+/*  Exercise 5.13: Each of the programs in the highlighted text on page 184 contains a common programming error. 
+    Identify and correct each error.    */
+
+/*  (a) unsigned aCnt = 0, eCnt = 0, iouCnt = 0;
+    char ch = next_text();
+    switch (ch) {
+        case 'a': aCnt++;
+        case 'e': eCnt++;
+        default: iouCnt++;
+    }
+    Error: There's no break after each case so all the cases will be executed.
+    
+    Correction: 
+    unsigned aCnt = 0, eCnt = 0, iouCnt = 0;
+    char ch = next_text();
+    switch (ch) {
+        case 'a': aCnt++;
+                  break:
+        case 'e': eCnt++;
+                  break:
+        default: iouCnt++;
+                  break:
+    }
+
+    (b) unsigned index = some_value();
+    switch (index) {
+    case 1:
+        int ix = get_value();
+        ivec[ ix ] = index;
+        break;
+    default:
+        ix = ivec.size()-1;
+        ivec[ ix ] = index;
+    }
+    Error: control bypass an explicitly initialized variable ix.
+
+    Correction:
+    unsigned index = some_value();
+    int ix;
+    switch (index) {
+    case 1:
+        ix = get_value();
+        ivec[ ix ] = index;
+        break;
+    default:
+        ix = ivec.size()-1;
+        ivec[ ix ] = index;
+    }
+
+    (c) unsigned evenCnt = 0, oddCnt = 0;
+    int digit = get_num() % 10;
+    switch (digit) {
+    case 1, 3, 5, 7, 9:
+        oddcnt++;
+        break;
+    case 2, 4, 6, 8, 10:
+        evencnt++;
+        break;
+    }
+    Error: case label syntax error.
+
+    Correction:
+    unsigned evenCnt = 0, oddCnt = 0;
+    int digit = get_num() % 10;
+    switch (digit) {
+    case 1: 3: 5: 7: 9:
+        oddcnt++;
+        break;
+    case 2: 4: 6: 8: 10:
+        evencnt++;
+        break;
+    }
+
+    (d) unsigned ival=512, jval=1024, kval=4096;
+    unsigned bufsize;
+    unsigned swt = get_bufCnt();
+    switch(swt) {
+        case ival:
+        bufsize = ival * sizeof(int);
+        break;
+    case jval:
+        bufsize = jval * sizeof(int);
+        break;
+    case kval:
+        bufsize = kval * sizeof(int);
+        break;
+    }
+    Error: case label must be a constant expression.
+
+    Correction:
+    const unsigned ival=512, jval=1024, kval=4096;
+    unsigned bufsize;
+    unsigned swt = get_bufCnt();
+    switch(swt) {
+        case ival:
+        bufsize = ival * sizeof(int);
+        break;
+    case jval:
+        bufsize = jval * sizeof(int);
+        break;
+    case kval:
+        bufsize = kval * sizeof(int);
+        break;
+    }
