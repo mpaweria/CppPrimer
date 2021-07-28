ebf4d6fa5b19263682d8da9e55be427393d655ad
mpaweria <ms.paweria@gmail.com>
2021-07-28 22:54:28 +0530

    Solutions till 6.51

--- Chapter6.cpp
+++ Chapter6.cpp
@@ -914,3 +914,41 @@ Exercise 6.49: What is a candidate function? What is a viable function?     */
     (b) match void f(int).
     (c) match void f(int, int).
     (d) match void f(double, double = 3.14).        */
+
+
+/*  Exercise 6.51: Write all four versions of f. Each function should print a distinguishing message. Check your 
+    answers for the previous exercise. If your answers were incorrect, study this section until you understand why
+    your answers were wrong.    */
+
+// #include <iostream>
+// using std::cout; using std::endl;
+
+// void f()
+// {
+//     cout << "f()" << endl;
+// }
+
+// void f(int)
+// {
+//     cout << "f(int)" << endl;
+// }
+
+// void f(int, int)
+// {
+//     cout << "f(int, int)" << endl;
+// }
+
+// void f(double, double)
+// {
+//     cout << "f(double, double)" << endl;
+// }
+
+// int main()
+// {
+//     // f(2.56, 42); // error: 'f' is ambiguous.
+//     f(42);
+//     f(42, 0);
+//     f(2.56, 3.14);
+    
+//     return 0;
+// }
