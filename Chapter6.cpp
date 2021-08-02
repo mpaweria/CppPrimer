5753c923c7b0c0bfb5f8e216c748c226b4eab057
mpaweria <ms.paweria@gmail.com>
2021-08-02 20:06:36 +0530

    Solutions till 6.56

--- Chapter6.cpp
+++ Chapter6.cpp
@@ -1014,3 +1014,10 @@ vector whose elements have this function pointer type.      */
     int subtract(int a, int b) { return a - b; }
     int multiply(int a, int b) { return a * b; }
     int divide(int a, int b) { return b != 0 ? a / b : 0; }     */
+
+
+//  Exercise 6.56: Call each element in the vector and print their result.
+
+// std::vector<decltype(func) *> vec{ add, subtract, multiply, divide };
+// for (auto f : vec)
+//           std::cout << f(2, 2) << std::endl;
