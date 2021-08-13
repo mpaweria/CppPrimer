ea4f81b3ac8488e714735e37c3877f2682b67a16
mpaweria <ms.paweria@gmail.com>
2021-08-13 23:31:23 +0530

    Solutions till 7.13

--- Chapter7.cpp
+++ Chapter7.cpp
@@ -205,3 +205,35 @@ Exercise 7.11: Add constructors to your Sales_data class and write a program to 
     Sales_data class        */
 
 //  will do tomorrow 
+
+
+//  Exercise 7.13: Rewrite the program from page 255 to use the istream constructor.
+
+// #include "ex7_12.h"
+
+// int main()
+// {
+//     Sales_data total(std::cin);
+//     if (!total.isbn().empty())
+//     {
+//         std::istream &is = std::cin;
+//         while (is) {
+//             Sales_data trans(is);
+//             if (!is) break;
+//             if (total.isbn() == trans.isbn())
+//                 total.combine(trans);
+//             else {
+//                 print(std::cout, total) << std::endl;
+//                 total = trans;
+//             }
+//         }
+//         print(std::cout, total) << std::endl;
+//     }
+//     else
+//     {
+//         std::cerr << "No data?!" << std::endl;
+//         return -1;
+//     }
+    
+//     return 0;
+// }
